1355417c8ac53208aaa4a9947ee9f34ea19a27f8
Shra6544 <shra6544@colorado.edu>
2024-10-29 15:28:21 -0600

    fixing errors 1.0

--- js/ica10.js
+++ js/ica10.js
@@ -1,6 +1,5 @@
 document.getElementById('colorButton').addEventListener('click', function() {
-    document.body.style.backgroundColor = 
-      document.body.style.backgroundColor === 'lightblue' ? 'white' : 'lightblue';
+    document.body.style.backgroundColor = document.body.style.backgroundColor === 'lightblue' ? 'white' : 'lightblue';
 });
   
 document.getElementById('toggleImage').addEventListener('mouseover', function() {
