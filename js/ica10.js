b163613167db250469405ba85c8871fde55c5c73
Shra6544 <shra6544@colorado.edu>
2024-10-29 15:16:56 -0600

    first commit finished?

---
+++ js/ica10.js
@@ -0,0 +1,12 @@
+document.getElementById('colorButton').addEventListener('click', function() {
+    document.body.style.backgroundColor = 
+      document.body.style.backgroundColor === 'lightblue' ? 'white' : 'lightblue';
+});
+  
+document.getElementById('toggleImage').addEventListener('mouseover', function() {
+    document.getElementById('toggleText').textContent = "You're hovering over the image!";
+});
+  
+document.getElementById('toggleImage').addEventListener('mouseout', function() {
+    document.getElementById('toggleText').textContent = "Hover over the image to change this text!";
+});
